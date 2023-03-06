bec0140986ee942952d1e99fa0190096770c1bc9
mwblevins <michaelwblevins@gmail.com>
2023-03-06 12:36:04 -0800

    task 2 test

--- models/base_model.py
+++ models/base_model.py
@@ -15,15 +15,14 @@ class BaseModel:
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
         self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
+        self.created_at = self.updated_at = datetime.now()
         if kwargs:
-            for k, v in kwargs.items():
-                if k in ('created_at', 'updated_at'):
-                    v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
-                if k != '__class__':
-                    setattr(self, k, v)
+            for ky, vl in kwargs.items():
+                if ky in ('created_at', 'updated_at'):
+                    vl = datetime.strptime(vl, '%Y-%m-%dT%H:%M:%S.%f')
+                if ky != '__class__':
+                    setattr(self, ky, vl)
         
     def __str__(self):
         """Returns a string representation of the instance"""
