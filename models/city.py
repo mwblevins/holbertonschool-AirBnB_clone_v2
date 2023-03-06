c83c06fb4a97c6f84845a0e6dd381c380baed1cf
mwblevins <michaelwblevins@gmail.com>
2023-03-06 14:05:57 -0800

    task8

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from os import getenv
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -10,3 +11,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='all, delete, delete-orphan')
--- models/place.py
+++ models/place.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60))
-    user_id = Column(String(60))
-    name = Column(String(128))
-    description = Column(String(1024))
-    number_rooms = Column(int, default=0, nullable=False)
-    number_bathrooms = Column(int, default=0, nullable=False)
-    max_guest = Column(int, default=0, nullable=False)
-    price_by_night = Column(int, default=0, nullable=False)
-    latitude = Column(float, nullable=True)
-    longitude = Column(float, nullable=True)
+    city_id = Column(String(60), ForeignKey("cities.id"),nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
