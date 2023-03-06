720cabcdbb13304e00bb23dfbbe9f42e20c52836
mwblevins <michaelwblevins@gmail.com>
2023-03-06 13:46:32 -0800

    tabel creation

--- models/place.py
+++ models/place.py
@@ -6,6 +6,7 @@ from sqlalchemy import Column, String
 
 class Place(BaseModel):
     """ A place to stay """
+    __tablename__ = 'places'
     city_id = Column(String(60))
     user_id = Column(String(60))
     name = Column(String(128))
