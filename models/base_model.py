63ee8345d4284c329bc6cc130110d1e5e3828d5a
jbees918 <5590@holbertonstudents.com>
2023-03-07 09:37:03 -0800

    fixed whitespace issue in basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,11 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """A base class for all the hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-    
+
     def __init__(self, *args, **kwargs):
         self.id = str(uuid4())
         self.created_at = self.updated_at = datetime.now()
