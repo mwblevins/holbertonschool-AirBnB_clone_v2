05200c1d18a0c0f5464b30ab0d466374eda8c4b2
jbees918 <5590@holbertonstudents.com>
2023-03-06 18:49:37 -0800

    updates for task 9

--- models/review.py
+++ models/review.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Culumn, String, ForeignKey
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ 
+    Review class for storing review information
+    """
+    __tablename__ = "reviews"
+    place_id = Column(String(1024), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
