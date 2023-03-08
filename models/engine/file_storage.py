75eb7696c2dc061428aacedabbda84b9236d861d
mwblevins <michaelwblevins@gmail.com>
2023-03-08 07:25:25 -0800

    More syntax

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,26 +45,26 @@ class FileStorage:
         self.save()
 
 
-def reload(self):
-    """Loads storage dictionary from file"""
-    from models.base_model import BaseModel
-    from models.user import User
-    from models.place import Place
-    from models.state import State
-    from models.city import City
-    from models.amenity import Amenity
-    from models.review import Review
+    def reload(self):
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
-    classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-    try:
-        temp = {}
-        with open(FileStorage.__file_path, 'r') as f:
-            temp = json.load(f)
-            for key, val in temp.items():
-                self.all()[key] = classes[val['__class__']](**val)
-    except FileNotFoundError:
-        pass
+        classes = {
+                        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                        'State': State, 'City': City, 'Amenity': Amenity,
+                        'Review': Review
+                    }
+        try:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
+            pass
