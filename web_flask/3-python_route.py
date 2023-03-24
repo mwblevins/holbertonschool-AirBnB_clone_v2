0a3af31b6f1f1ff80ed0e6277ccf4199e31bbc31
mwblevins <michaelwblevins@gmail.com>
2023-03-24 13:33:41 -0700

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Start Flask"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', methods=['GET'], strict_slashes=False)
+def c(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={'text': 'is cool'}, methods=['GET'],
+           strict_slashes=False)
+@app.route('python/<text', methods=['GET'], strict_slashes=False)
+def python(text):
+    return "Python {}".format(text.replace("_", " "))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
