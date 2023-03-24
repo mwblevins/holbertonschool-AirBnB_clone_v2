f2f8ee166bc7a377a3f974b9ee1aa0d80920f71a
mwblevins <michaelwblevins@gmail.com>
2023-03-24 13:52:12 -0700

    Added more documentation

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,16 +8,19 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', methods=['GET'], strict_slashes=False)
 def hbnb():
+    """Display 'HBNB' """
     return "HBNB"
 
 
 @app.route('/c/<text>', methods=['GET'], strict_slashes=False)
 def c(text):
+    """Display 'C' followed by text"""
     return "C {}".format(text.replace("_", " "))
 
 
@@ -25,7 +28,9 @@ def c(text):
            strict_slashes=False)
 @app.route('python/<text>', methods=['GET'], strict_slashes=False)
 def python(text):
+    """Display 'Python' Followed by text"""
     return "Python {}".format(text.replace("_", " "))
 
 if __name__ == '__main__':
+    """Start Flask web application"""
     app.run(host='0.0.0.0', port=5000)
