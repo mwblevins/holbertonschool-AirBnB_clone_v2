dee1e5564c106aaff8435973cb1399d74544cea9
mwblevins <michaelwblevins@gmail.com>
2023-03-26 10:20:24 -0700

    task 7

--- models/state.py
+++ models/state.py
@@ -4,10 +4,23 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from os import getenv
 from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state",
+                              cascade="delete")
+    else:
+        @property
+        def cities(self):
+            """uhhhhhh"""
+            city_list = []
+            for x in models.storage.all(City).values():
+                if x.state_id == self.id:
+                    city_list.append(x)
+            return city_list
