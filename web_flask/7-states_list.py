a4618ce88edad1e569c4cfa86b0d7d049f3b117d
mwblevins <michaelwblevins@gmail.com>
2023-03-27 16:52:26 -0700

    Task77

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
-"""Start Flask application for Holberton task 7"""
-
-from flask import Flask, render_template
+"""
+You must use storage for fetching data from
+the storage engine (FileStorage or DBStorage) =>
+from models import storage and storage.all(...)
+"""
 from models import storage
-from models.state import State
-
+from flask import render_template, Flask
 
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+    /states_list: display a HTML page: (inside the tag BODY)
+    """
+    states = storage.all('State')
+    return render_template("7-states_list.html", states=states)
+
+
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown(exc):
+    """
+    After each request you must remove the current SQLAlchemy Session:
+    Declare a method to handle @app.teardown_appcontext
+    Call in this method storage.close()
+    """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda s: s.name)
-    return render_template('states_list.html', states=sorted_states)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
