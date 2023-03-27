57baee4e09d58757c378847713480f61f7d23384
mwblevins <michaelwblevins@gmail.com>
2023-03-27 16:03:52 -0700

    Task7 again

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """Start Flask application for Holberton task 7"""
 
-from models import storage
 from flask import Flask, render_template
+from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """States list to display html page"""
-    states = storage.all('State')
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove current sqlalchemy session affter each request..."""
+def teardown_appcontext(exception):
     storage.close()
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda s: s.name)
+    return render_template('states_list.html', states=sorted_states)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0.', port=5000)
+    app.run(host='0.0.0.0', port=5000)
